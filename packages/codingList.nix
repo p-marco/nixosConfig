19ed0f695140931f0b41d6f7dd47e4686a3dc0f9
p-marco <marco.petolicchio@gmail.com>
2019-05-17 17:33:25 +0200

    update R

--- packages/codingList.nix
+++ packages/codingList.nix
@@ -1,8 +1,50 @@
 { config, pkgs, ... }:
 
+
+with pkgs;
+let
+  R-with-my-packages = rWrapper.override{ packages = with rPackages; [ 
+    bookdown
+    data.table
+    devtools
+    dplyr
+    ggplot2 
+    grid
+    gtable
+    htmltools
+    jsonlite
+    kableExtra
+    KernSmooth
+    Knitr
+    labeling
+    lazyeval
+    magrittr
+    markdown
+    openssl
+    parallel
+    Rcpp
+    reshape2
+    revealjs
+    rmarkdown
+    tidyr
+    tidyselect
+    tinytex
+    udpipe
+    utf8
+    utils
+    xml2
+    xopen
+    yaml
+  ]; 
+};
+in 
+
+
 {
 
     environment.systemPackages = with pkgs; [
         texlive.combined.scheme-full
+        R-with-my-packages
     ];
 }
+
