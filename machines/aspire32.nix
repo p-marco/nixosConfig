de0e05c6b121f99b09dbc7489a9d3533d0e4ee74
p-marco <marco.petolicchio@gmail.com>
2019-05-20 07:02:27 +0200

    added aspire 32 bit

---
+++ machines/aspire32.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [   # Include the results of the hardware scan.
+        ../hardware-configuration.nix
+        # Include base config
+        ../configs/base.nix 
+        # Include packages for root
+        ../packages/base.nix
+        # Include fonts
+        ../fonts/base.nix
+        # Include users
+        ../users/base.nix
+        ../users/guest.nix
+        # Include Bootloader
+        ../boot/grub.nix
+        # Include Desktop
+        ../desktops/i3.nix
+        # ../desktops/compton.nix
+        # Include Kernel
+        ../kernels/linux-hardened.nix
+    ];
+
+  # Boot.
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/sda3";
+      preLVM = true;
+    }
+  ];
+
+  # Networking.
+  networking.hostName = "aspire32"; 
+  networking.wireless.enable = true;
+  services.xserver.synaptics.enable = true;
+  programs.light.enable = true;
+}
+
+
