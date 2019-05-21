e4705baacc95be55d0af221efa33827e976487fd
p-marco <marco.petolicchio@gmail.com>
2019-05-21 08:03:20 +0200

    l

--- machines/aspire32.nix
+++ machines/aspire32.nix
@@ -38,7 +38,7 @@
   programs.light.enable = true;
 
   nixpkgs.config.allowUnsupportedSystem = true;
-  stdenv.userHook = '' NIX_CFLAGS_COMPILE+=" -march=native -O2" '';
+  nixpkgs.config.stdenv.userHook = '' NIX_CFLAGS_COMPILE+=" -march=native -O2" '';
 }
 
 
