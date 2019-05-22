3b3642e972a48f0a3d4495a139c40d32cfef0abe
p-marco <marco.petolicchio@gmail.com>
2019-05-22 09:31:00 +0200

    .

--- hardware/extDevices.nix
+++ hardware/extDevices.nix
@@ -5,8 +5,8 @@
 
   fileSystems."/media/Elements" =
     { device = "/dev/disk/by-uuid/4E1AEA7B1AEA6007";    
-      fsType = "ntfs";
-      options = ["user" "uid=1000" "rw"];
+      fsType = "ntfs-3g";
+      options = ["user" "uid=1000" "rw" "noauto"];
     };
   
 }
