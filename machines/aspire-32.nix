5304eca429a431941eefa569932439e5b6fd049f
p-marco <marco.petolicchio@gmail.com>
2019-05-30 10:12:21 +0200

    Update aspire-32.nix

--- machines/aspire-32.nix
+++ machines/aspire-32.nix
@@ -43,6 +43,7 @@
   services.xserver.desktopManager.default = "none";
   services.xserver.windowManager.default = "i3";
   services.xserver.windowManager.openbox.enable = true;
+  services.xserver.windowManager.i3.enable = true;
   
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.lightdm.autoLogin.enable = true;
