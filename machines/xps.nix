047cddaaec35723258b2e2d42651a1b58a5b244a
p-marco <marco.petolicchio@gmail.com>
2019-12-30 22:26:31 +0100

    xps

--- machines/xps.nix
+++ machines/xps.nix
@@ -7,7 +7,7 @@
       # Include bootloader :
       ../boot/efi.nix
       # General configuration :
-      ../configs/base.nix
+      # ../configs/base.nix
       # Packages :
       ../packages/base.nix
       # Fonts : 
@@ -94,5 +94,64 @@
 
   # Enable the Oracle Extension Pack.
   nixpkgs.config.virtualbox.enableExtensionPack = true;
+
+
+
+{     
+    
+    # Boot.
+    boot.initrd.luks.devices = [
+      {
+        name = "root";
+        device = "/dev/sda3";
+        preLVM = true;
+      }
+    ];
+
+    # Internationalization.
+    i18n = {
+      consoleFont = "Source Code Pro";
+      consoleKeyMap = "it";
+      defaultLocale = "en_US.UTF-8";
+    };
+    
+    # Networking.
+    networking.dhcpcd.extraConfig = "noarp";
+
+    # Services.
+    services = {
+      fstrim = {
+        enable = true;
+      };
+      xserver = {
+        enable = true;
+        xkbOptions = "eurosign:e";
+        layout = "it,us,cz,sk";
+      };
+    };
+    
+    #System.
+    system = {
+      autoUpgrade = {
+        enable = true;
+      };
+      stateVersion = "19.09";
+    }; 
+    
+    # Timezone.
+    time.timeZone = "Europe/Prague";
+
+
+
+
+    # services.fstrim.enable = true;
+    # 
+    # services.xserver.enable = true;
+    # services.xserver.xkbOptions = "eurosign:e";
+    # services.xserver.layout = "it,us,cz,sk";
+
+}
+
+
 
 }
